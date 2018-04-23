62204f3de03042e5d3e7601c97b2ed539d54efcc
Vernon Fraser <vernon.jfraser@yahoo.com>
2018-04-22 21:04:12 -0400

    Added Redux action to handle UI events

---
+++ src/actions/actions.test.js
@@ -0,0 +1,16 @@
+import actions from './actions';
+import types from '../constants/constants';
+
+describe('Actions', ()=> {
+    const todoText =  'A todo';
+
+    it('should create an action to add a todo', ()=>{
+        const expectedAction = {
+            type: types.SUBMIT_TODO,
+            id: 1,
+            text: todoText
+        }
+
+        expect(actions.submitTodo(todoText)).toEqual(expectedAction);
+    })
+})
