f63f70886d8c1d3ed6f33cdae140b1cb00a4f856
Vernon Fraser <vernon.jfraser@yahoo.com>
2018-04-22 21:46:40 -0400

    added Redux store

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { combineReducers, createStore } from "redux";
+import todoListApp from './reducers'
+
+const reducers = combineReducers({
+    todoListApp,
+
+});
+
+export default createStore(reducers);
